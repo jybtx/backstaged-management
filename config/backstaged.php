940e2e4dcc29b1741c1164f9f0a5d5d03c4c4ba5
游遍天下 <34736990+jybtx@users.noreply.github.com>
2019-11-27 11:19:40 +0800

    add config file

---
+++ config/backstaged.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+
+ 	/*
+    |--------------------------------------------------------------------------
+    | background management system route setting
+    |--------------------------------------------------------------------------
+    |
+    | The routing configuration of the admin page, including the path prefix,
+    | the controller namespace, and the default middleware. If you want to
+    | access through the root path, just set the prefix to empty string.
+    |
+    */
+    'route' => [
+
+        'prefix' => env('ADMIN_ROUTE_PREFIX', 'admin'),
+
+        'namespace' => 'App\\Http\\Backstaged\\Controllers',
+
+        'middleware' => ['web', 'admin'],
+    ],
+    /*
+    |--------------------------------------------------------------------------
+    | background management system auth setting
+    |--------------------------------------------------------------------------
+    |
+    | Authentication settings for all admin pages. Include an authentication
+    | guard and a user provider setting of authentication driver.
+    |
+    | You can specify a controller for `login` `logout` and other auth routes.
+    |
+    */
+    'auth'  => [
+    	'guard' => 'admin',
+    	'guares' => [
+    		'admin' => [
+				'driver'   => 'session',
+				'provider' => 'admins',
+    		],
+    	],
+    	'providers'  => [
+    		'admins' => [
+				'driver' => 'eloquent',
+				'model'  => Jybtx\Backstaged\Models\Admin::class,
+	        ],
+    	],
+    ],
+
+];
